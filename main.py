8b1cb3478a9a78b4ad140ff4bdf742330445ccc2
Christopher Bishop <domingobishop@gmail.com>
2017-12-05 15:13:20 +0000

    tweeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # import libraries
+import tweepy
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -83,4 +84,27 @@ def get_guardian_summary(n):
     return articles_summary
 
 
-print(get_guardian_summary(1))
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+
+def main():
+  # Fill in the values noted in previous step here
+  cfg = {
+    "consumer_key"        : "",
+    "consumer_secret"     : "",
+    "access_token"        : "",
+    "access_token_secret" : ""
+    }
+
+  tweet_list = get_guardian_summary(1)
+
+  api = get_api(cfg)
+  tweet = tweet_list[0][0] + ' ' + ' #' + tweet_list[1] + ' #' + tweet_list[2] + ' #' + tweet_list[3]
+  status = api.update_status(status=tweet)
+
+
+if __name__ == "__main__":
+  main()
