3a6fef283669c0346acfe1223e861e91259b18ee
Christopher Bishop <domingobishop@gmail.com>
2017-12-07 11:53:18 +0000

    training data

--- functions.py
+++ functions.py
@@ -28,50 +28,56 @@ def get_article(url, element, att, value):
     article = soup.find(element, attrs={att: value})
     if article:
         article = clean_text(article)
-        return article
+        return article + ' '
     else:
-        return ' '
+        return ''
 
 
-def summarize(text, n):
-    sentences = sent_tokenize(text)
-    sentences = list(set(sentences))
-    sentences = [x for x in sentences if len(x) < 110]
+def summarize(text, n, training=False):
+    if len(text) > 110:
+        sentences = sent_tokenize(text)
+        sentences = list(set(sentences))
+        sentences = [x for x in sentences if len(x) < 110]
 
-    my_stopwords = [
-        'would',
-        'said',
-        'one',
-        'new',
-        'also',
-        'read',
-        'time',
-        'people',
-        'says',
-        'like',
-        'us',
-        'years'
-    ]
+        my_stopwords = [
+            'would',
+            'said',
+            'one',
+            'new',
+            'also',
+            'read',
+            'time',
+            'people',
+            'says',
+            'like',
+            'us',
+            'years'
+        ]
 
-    assert n <= len(sentences)
-    words = word_tokenize(text.lower())
-    _stopwords = set(stopwords.words('english') + list(punctuation) + my_stopwords)
+        assert n <= len(sentences)
+        words = word_tokenize(text.lower())
+        _stopwords = set(stopwords.words('english') + list(punctuation) + my_stopwords)
 
-    new_words = [word for word in words if word not in _stopwords]
-    freq = FreqDist(new_words)
+        new_words = [word for word in words if word not in _stopwords]
+        freq = FreqDist(new_words)
 
-    top_3 = nlargest(3, freq, key=freq.get)
+        top_3 = nlargest(3, freq, key=freq.get)
 
-    ranking = defaultdict(int)
+        ranking = defaultdict(int)
 
-    for i, sent in enumerate(sentences):
-        for w in word_tokenize(sent.lower()):
-            if w in freq:
-                ranking[i] += freq[w]
+        for i, sent in enumerate(sentences):
+            for w in word_tokenize(sent.lower()):
+                if w in freq:
+                    ranking[i] += freq[w]
 
-    sents_idx = nlargest(n, ranking, key=ranking.get)
-    summary = [sentences[j] for j in sorted(sents_idx)]
-    return [summary, top_3[0], top_3[1], top_3[2]]
+        sents_idx = nlargest(n, ranking, key=ranking.get)
+        summary = [sentences[j] for j in sorted(sents_idx)]
+        if training:
+            return summary
+        else:
+            return [summary, top_3[0], top_3[1], top_3[2]]
+    else:
+        return ''
 
 
 def clean_text(article):
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # import libraries
 import tweet
 
+
 print("NLP Crawler 0.1")
 
 if __name__ == "__main__":
     tweet.main()
+
+# import training
+# print(training.get_guardian_comments_summary(1))
+
