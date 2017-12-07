9deee7d94427aa0d34b1124cbdb935b034cb475a
Christopher Bishop <domingobishop@gmail.com>
2017-12-07 15:24:55 +0000

    create classifier

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import nltk
 
 data = [
     ('The defendant has a leadership role within VW, federal officials said.', 'neutral'),
@@ -49,62 +50,95 @@ data = [
     ('I will also discuss with House officials the steps necessary to repay previous travel claims.', 'positive')
 ]
 
-tweets = []
-for (words, sentiment) in data:
-    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
-    tweets.append((words_filtered, sentiment))
 
-print(tweets)
+def create_training_data():
+    tweets = []
+    for (words, sentiment) in data:
+        words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
+        tweets.append((words_filtered, sentiment))
+    print(tweets)
+
 
 training_data = [
-    (['each', 'big', 'four', 'australian', 'banks', 'ruled', 'out', 'financing', 'mine.'], 'positive'),
-    (['fusion', 'power', 'most', 'sought-after', 'technological', 'goals', 'pursuit', 'clean', 'energy.'], 'positive'),
-    (['arguing', 'switch', 'electric', 'transport', 'will', 'drive', 'future', 'demand.'], 'positive'),
-    (['theres', 'lot', 'eyes', 'antarctica', 'right', 'now', 'lot', 'scientists', 'want', 'know', 'whats', 'going', 'happen.'], 'negative'),
-    (['winter', 'riding', 'rural', 'areas', 'can', 'more', 'tricky,', 'but', 'even', 'modern', 'lights', 'can', 'make', 'much', 'easier.'], 'positive'),
-    (['chung', 'said', 'traffic', 'pollution', 'might', 'also', 'affect', 'younger', 'people', 'doing', 'their', 'shopping', 'heart', 'city.'], 'negative'),
-    (['food', 'sustainability', 'map', 'crossley', 'said', 'highlighted', 'challenge', 'facing', 'brexit', 'approached.'], 'negative'),
-    (['north', 'alaska', 'countrys', 'last', 'frontier,', 'hes', 'letting', 'oil', 'industry', 'suck', 'life', 'out', 'it.'], 'negative'),
-    (['what', 'report', 'also', 'shows', 'biggest', 'uncertainty', 'future', 'climate', 'change', 'us.'], 'negative'),
-    (['2015', 'paris', 'accord', 'recognised', 'contribution', 'indigenous', 'knowledge', 'dealing', 'with', 'climate', 'change.'], 'positive'),
-    (['society', 'can', 'afford', 'ignore', 'air', 'pollution.'], 'negative'),
-    (['facebook', 'car', 'was', 'described', 'clean', 'car', 'helps', 'give', 'back', 'environment.'], 'positive'),
+    (['big', 'four', 'australian', 'banks', 'ruled', 'out', 'financing'], 'positive'),
+    (['fusion', 'power', 'most', 'sought-after', 'technological', 'goals', 'pursuit', 'clean', 'energy'], 'positive'),
+    (['arguing', 'switch', 'electric', 'transport', 'will', 'drive', 'future', 'demand'], 'positive'),
+    (['theres', 'lot', 'eyes', 'antarctica', 'right', 'now', 'lot', 'scientists', 'want', 'know', 'whats', 'going', 'happen'], 'negative'),
+    (['winter', 'riding', 'rural', 'areas', 'can', 'more', 'tricky,', 'but', 'even', 'modern', 'lights', 'can', 'make', 'much', 'easier'], 'positive'),
+    (['chung', 'said', 'traffic', 'pollution', 'might', 'also', 'affect', 'younger', 'people', 'doing', 'their', 'shopping', 'heart', 'city'], 'negative'),
+    (['food', 'sustainability', 'map', 'crossley', 'said', 'highlighted', 'challenge', 'facing', 'brexit', 'approached'], 'negative'),
+    (['north', 'alaska', 'countrys', 'last', 'frontier,', 'hes', 'letting', 'oil', 'industry', 'suck', 'life', 'out', 'it'], 'negative'),
+    (['what', 'report', 'also', 'shows', 'biggest', 'uncertainty', 'future', 'climate', 'change', 'us'], 'negative'),
+    (['2015', 'paris', 'accord', 'recognised', 'contribution', 'indigenous', 'knowledge', 'dealing', 'with', 'climate', 'change'], 'positive'),
+    (['society', 'can', 'afford', 'ignore', 'air', 'pollution'], 'negative'),
+    (['facebook', 'car', 'was', 'described', 'clean', 'car', 'helps', 'give', 'back', 'environment'], 'positive'),
     (['ask', 'how', 'deal', 'with', 'plastic', 'waste', 'without', 'exporting'], 'neutral'),
-    (['alberto', 'bai,', 'matsés', 'traditional', 'healer,', 'healing', 'forest', 'near', 'buenas', 'lomas', 'nueva', 'perus', 'amazon.'], 'neutral'),
-    (['liberating', 'earth', 'means', 'defending', 'land,', 'says', 'josé', 'rene', 'guetio,', 'nasa', 'elder.'], 'positive'),
+    (['alberto', 'bai,', 'matsés', 'traditional', 'healer,', 'healing', 'forest', 'near', 'buenas', 'lomas', 'nueva', 'perus', 'amazon'], 'neutral'),
+    (['liberating', 'earth', 'means', 'defending', 'land,', 'says', 'josé', 'rene', 'guetio,', 'nasa', 'elder'], 'positive'),
     (['will', 'norman', 'londons', 'walking', 'cycling', 'commissioner'], 'neutral'),
-    (['because', 'polar', 'bears', 'rely', 'sea', 'ice', 'hunt', 'seals,', 'global', 'warming', 'also', 'threatens', 'their', 'species.'], 'negative'),
-    (['global', 'change', 'research', 'program', 'recently', 'released', 'climate', 'science', 'special', 'report.'], 'neutral'),
-    (['first', 'main', 'chapter', 'deals', 'with', 'changes', 'climate', 'focuses', 'much', 'attention', 'global', 'temperatures.'], 'neutral'),
-    (['society', 'can', 'afford', 'ignore', 'air', 'pollution.'], 'negative'),
-    (['luxembourg-based', 'court', 'justice', 'european', 'union', 'highest', 'court', 'europe.'], 'neutral'),
-    (['brexit', 'makes', 'headlines', 'every', 'day', 'without', 'fail,', 'even', 'when', 'nothing', 'happened.'], 'negative'),
-    (['they', 'should', 'not', 'allow', 'popularity', 'term', 'populism', 'mask', 'nativism', 'radical', 'right.'], 'negative'),
-    (['november,', 'momentum', 'said', 'spent', '£38,743', 'general', 'election', 'campaign.'], 'neutral'),
-    (['quiet', 'meetings', 'brexit', 'took', 'place', 'between', 'civil', 'servants', 'ireland', 'northern', 'ireland.'], 'neutral'),
-    (['2016,', 'david', 'camerons', 'austerity', 'agenda', 'seen', 'child', 'poverty', 'rates', 'shoot', '30%.'], 'negative'),
-    (['dup', 'leader', 'also', 'sought', 'repair', 'some', 'damaged', 'relations', 'with', 'irish', 'government', 'wednesday.'], 'neutral'),
-    (['there', 'risk', 'tories', 'versus', 'police', 'row', 'will', 'have', 'domino', 'effect', 'her', 'cabinet.'], 'negative'),
-    (['britains', 'exit', 'from', 'taking', 'northern', 'ireland', 'with', 'risks', 'return', 'hard', 'policed', 'border.'], 'negative'),
-    (['yet', 'rather', 'stay', 'shoulder', 'shoulder', 'with', 'union,', 'british', 'chose', 'their', 'own', 'again.'], 'negative'),
-    (['‘the', 'tories', 'economic', 'strategy', 'failed', 'its', 'own', 'stated', 'terms.'], 'negative'),
-    (['adonis', 'criticised', 'johnson', 'twitter', 'failing', 'speak', 'out', 'against', 'high', 'pay', 'packets.'], 'negative'),
-    (['others', 'have', 'urged', 'government', 'ensure', 'funding', 'replaced', 'from', '2019', 'onwards.'], 'neutral'),
-    (['health', 'safety', 'executives', 'myth-busting', 'website', 'rebut', 'such', 'nonsense', 'every', 'week.'], 'negative'),
-    (['experience', 'employing', 'disabled', 'people', 'they', 'are', 'brilliant', 'employees.'], 'positive'),
-    (['new', 'defence', 'secretary', 'told', 'daily', 'mail:', 'dead', 'terrorist', 'cant', 'cause', 'any', 'harm', 'britain.'], 'negative'),
-    (['meeting', 'discuss', 'them', 'was', 'scheduled', 'may,', 'but', 'attack', 'manchester', 'arena', 'happened', 'may.'], 'negative'),
-    (['number', 'big,', 'unanswered', 'questons', 'about', 'acos', 'remain,', 'despite', 'their', 'imminent', 'arrival', 'nhs,', 'adds.'], 'neutral'),
-    (['london', 'depends', 'foreign', 'workers', 'keep', 'growing', 'therefore', 'keep', 'boosting', 'tax', 'export.'], 'positive'),
-    (['there', 'were', 'signs', 'progress', 'was', 'being', 'made', 'towards', 'deal', 'avoid', 'hard', 'border', 'island', 'ireland.'], 'positive'),
-    (['will', 'also', 'discuss', 'with', 'house', 'officials', 'steps', 'necessary', 'repay', 'previous', 'travel', 'claims.'], 'positive')
+    (['because', 'polar', 'bears', 'rely', 'sea', 'ice', 'hunt', 'seals,', 'global', 'warming', 'also', 'threatens', 'their', 'species'], 'negative'),
+    (['global', 'change', 'research', 'program', 'recently', 'released', 'climate', 'science', 'special', 'report'], 'neutral'),
+    (['first', 'main', 'chapter', 'deals', 'with', 'changes', 'climate', 'focuses', 'much', 'attention', 'global', 'temperatures'], 'neutral'),
+    (['society', 'can', 'afford', 'ignore', 'air', 'pollution'], 'negative'),
+    (['luxembourg-based', 'court', 'justice', 'european', 'union', 'highest', 'court', 'europe'], 'neutral'),
+    (['brexit', 'makes', 'headlines', 'every', 'day', 'without', 'fail,', 'even', 'when', 'nothing', 'happened'], 'negative'),
+    (['they', 'should', 'not', 'allow', 'popularity', 'term', 'populism', 'mask', 'nativism', 'radical', 'right'], 'negative'),
+    (['november,', 'momentum', 'said', 'spent', '£38,743', 'general', 'election', 'campaign'], 'neutral'),
+    (['quiet', 'meetings', 'brexit', 'took', 'place', 'between', 'civil', 'servants', 'ireland', 'northern', 'ireland'], 'neutral'),
+    (['2016,', 'david', 'camerons', 'austerity', 'agenda', 'seen', 'child', 'poverty', 'rates', 'shoot', '30%'], 'negative'),
+    (['dup', 'leader', 'also', 'sought', 'repair', 'some', 'damaged', 'relations', 'with', 'irish', 'government', 'wednesday'], 'neutral'),
+    (['there', 'risk', 'tories', 'versus', 'police', 'row', 'will', 'have', 'domino', 'effect', 'her', 'cabinet'], 'negative'),
+    (['britains', 'exit', 'from', 'taking', 'northern', 'ireland', 'with', 'risks', 'return', 'hard', 'policed', 'border'], 'negative'),
+    (['yet', 'rather', 'stay', 'shoulder', 'shoulder', 'with', 'union,', 'british', 'chose', 'their', 'own', 'again'], 'negative'),
+    (['‘the', 'tories', 'economic', 'strategy', 'failed', 'its', 'own', 'stated', 'terms'], 'negative'),
+    (['adonis', 'criticised', 'johnson', 'twitter', 'failing', 'speak', 'out', 'against', 'high', 'pay', 'packets'], 'negative'),
+    (['others', 'have', 'urged', 'government', 'ensure', 'funding', 'replaced', 'from', '2019', 'onwards'], 'neutral'),
+    (['health', 'safety', 'executives', 'myth-busting', 'website', 'rebut', 'such', 'nonsense', 'every', 'week'], 'negative'),
+    (['experience', 'employing', 'disabled', 'people', 'they', 'are', 'brilliant', 'employees'], 'positive'),
+    (['new', 'defence', 'secretary', 'told', 'daily', 'mail:', 'dead', 'terrorist', 'cant', 'cause', 'any', 'harm', 'britain'], 'negative'),
+    (['meeting', 'discuss', 'them', 'was', 'scheduled', 'may,', 'but', 'attack', 'manchester', 'arena', 'happened', 'may'], 'negative'),
+    (['number', 'big,', 'unanswered', 'questons', 'about', 'acos', 'remain,', 'despite', 'their', 'imminent', 'arrival', 'nhs,', 'adds'], 'neutral'),
+    (['london', 'depends', 'foreign', 'workers', 'keep', 'growing', 'therefore', 'keep', 'boosting', 'tax', 'export'], 'positive'),
+    (['there', 'were', 'signs', 'progress', 'was', 'being', 'made', 'towards', 'deal', 'avoid', 'hard', 'border', 'island', 'ireland'], 'positive'),
+    (['will', 'also', 'discuss', 'with', 'house', 'officials', 'steps', 'necessary', 'repay', 'previous', 'travel', 'claims'], 'positive')
 ]
 
 test_data = [
-    (['defendant', 'leadership', 'role', 'within', 'federal', 'officials', 'said.'], 'neutral'),
-    (['uks', 'spending', 'watchdog', 'called', 'project', 'risky', 'expensive.'], 'negative'),
-    (['initiative', 'global', 'markets', 'panel', 'economic', 'experts', 'was', 'recently', 'asked', 'about', 'republican', 'tax', 'plan.'], 'neutral'),
-    (['gwr', 'unveiled', 'its', 'new', 'trains', 'october,', 'with', 'two', 'hanging', 'bike', 'storage', 'spaces', 'per', 'carriage.'], 'positive'),
+    (['defendant', 'leadership', 'role', 'within', 'federal', 'officials', 'said'], 'neutral'),
+    (['uks', 'spending', 'watchdog', 'called', 'project', 'risky', 'expensive'], 'negative'),
+    (['initiative', 'global', 'markets', 'panel', 'economic', 'experts', 'was', 'recently', 'asked', 'about', 'republican', 'tax', 'plan'], 'neutral'),
+    (['gwr', 'unveiled', 'its', 'new', 'trains', 'october,', 'with', 'two', 'hanging', 'bike', 'storage', 'spaces', 'per', 'carriage'], 'positive'),
     (['days', 'london', 'smogs', 'was', 'possible', 'both', 'see', 'pollution', 'smell'], 'negative'),
-    (['there', 'was', 'sense', 'disbelief', 'been', 'close,', 'clumsy', 'intruder', 'among', 'wild', 'tangle', 'trees.'], 'negative')
+    (['there', 'was', 'sense', 'disbelief', 'been', 'close,', 'clumsy', 'intruder', 'among', 'wild', 'tangle', 'trees'], 'negative')
 ]
+
+
+def get_words_in_tweets(data):
+    all_words = []
+    for (words, sentiment) in data:
+      all_words.extend(words)
+    return all_words
+
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
+
+
+word_features = get_word_features(get_words_in_tweets(training_data))
+
+training_set = nltk.classify.apply_features(extract_features, training_data)
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+tweet = 'There are many young people among the Matsés, but the majority dont want to know.'
+print(classifier.classify(extract_features(tweet.split())))
